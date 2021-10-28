74eeaec52c0ceef46995647d8aaaace41dbc8435
Simon Thyregod Kristensen <git@simon.thyregod.eu>
2021-10-28 13:52:39 +0200

    Clean up gem dependency

--- Gemfile
+++ Gemfile
@@ -4,5 +4,6 @@ source 'https://rubygems.org'
 
 gem 'activesupport'
 gem 'faraday'
-gem 'rspec', '~> 3.0'
-gem 'simplecov'
+gem 'json'
+gem 'rspec', '~> 3.0', require: false
+gem 'simplecov', require: false
--- lib/shipping_connector.rb
+++ lib/shipping_connector.rb
@@ -1,9 +1,5 @@
 # frozen_string_literal: true
 
-require 'faraday'
-require 'json'
-require 'active_support/core_ext/class/attribute'
-
 require 'shipping_connector/carrier'
 require 'shipping_connector/carriers'
 require 'shipping_connector/service_point'
